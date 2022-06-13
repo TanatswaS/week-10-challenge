3543c6bcf47624e120f3ab47f90d8de0b72817fe
Mister Tanatswa <tanatswasingano@gmail.com>
2022-06-12 20:48:55 -0400

    added add manager and intern function

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test("Can create a github.", () => {
+    const testGithub = "TanatswaS";
+    const employeeInstance = new Engineer("Tanatswa", 2, "tanatswasingano@gmail.com", testGithub);
+    expect(employeeInstance.github).toBe(testGithub);
+})
+
+test("Testing getGithub will return github.", () => {
+    const testGithub = "TanatswaS";
+    const employeeInstance = new Engineer("Tanatswa", 2, "tanatswasingano@gmail.com", testGithub);
+    expect(employeeInstance.getGithub()).toBe(testGithub);
+})
+
+test("Testing role.", () => {
+    const returnValue = "Engineer";
+    const employeeInstance = new Engineer("Tanatswa", 2, "tanatswasingano@gmail.com", "TanatswaS");
+    expect(employeeInstance.getRole()).toBe(returnValue);
+})
