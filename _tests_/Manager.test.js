b1137c4db4d73eea2ae828ad95e0b491a99b0dc1
Mister Tanatswa <tanatswasingano@gmail.com>
2022-06-13 00:19:56 -0400

    Added code to template.js file

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -2,18 +2,18 @@ const Manager = require("../lib/Manager");
 
 test("Can create an office number.", () => {
     const testOfficeNumber = 2;
-    const employeeInstance = new Manager("James", 2, "jamesljenks@gmail.com", testOfficeNumber);
+    const employeeInstance = new Manager("Tanatswa", 2, "tanatswasingano@gmail.com", testOfficeNumber);
     expect(employeeInstance.officeNumber).toBe(testOfficeNumber);
 });
 
 test("Testing officeNumber will return office number.", () => {
-    const testGithub = 2;
-    const employeeInstance = new Manager("James", 2, "jamesljenks@gmail.com", testOfficeNumber);
+    const testOfficeNumber = 2;
+    const employeeInstance = new Manager("Tanatswa", 2, "tanatswasingano@gmail.com", testOfficeNumber);
     expect(employeeInstance.getOfficeNumber()).toBe(testOfficeNumber);
 });
 
 test("Testing role.", () => {
     const returnValue = "Manager";
-    const employeeInstance = new Manager("James", 2, "jamesljenks@gmail.com", 2);
+    const employeeInstance = new Manager("Tanatswa", 2, "tanatswasingano@gmail.com", 2);
     expect(employeeInstance.getRole()).toBe(returnValue);
 });
--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+const generateTeam = require("./src/template.js")
 
 teamArray = [];
 
 
 
-
-
-
-
-
 function runApp () {
-// Add OOP functions here - create for the other roles, but not Employee.
+
+  function createTeam () {
+    inquirer.prompt([{
+      type: "list",
+      message: "What type of employee would you like to add to your team?",
+      name: "addEmployeePrompt",
+      choices: ["Manager", "Engineer", "Intern", "No more team members are needed."]
+    }]).then(function (userInput) {
+      switch(userInput.addEmployeePrompt) {
+        case "Manager":
+          addManager();
+          break;
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+
+        default:
+          htmlBuilder();
+      }
+    })
+  }
+// OOP Functions
 
 function addManager() {
   inquirer.prompt ([
-
+    
     {
       type: "input",
       name: "managerName",
@@ -44,7 +63,7 @@ function addManager() {
     {
       type: "input",
       name: "managerOfficeNumber",
-      message: "What is the manager's office phone number?"
+      message: "What is the manager's office number?"
     }
 
   ]).then(answers => {
@@ -55,42 +74,45 @@ function addManager() {
 
 }
 
+
 function addEngineer() {
     inquirer.prompt([
+      
       {
         type: "input",
         name: "engineerName",
-        message: "What is your engineer's name?",
-
+        message: "What is the engineer's name?"
       },
+
       {
         type: "input",
         name: "engineerId",
-        message: "What is your engineer's employee id?",
-
+        message: "What is the engineer's employee ID number?" 
       },
+
       {
         type: "input",
         name: "engineerEmail",
-        message: "What is your engineer's email address?",
-
+        message: "What is the engineer's email address?"
       },
+
       {
         type: "input",
         name: "engineerGithub",
-        message: "What is your engineer's GitHub username?",
-
+        message: "What is the engineer's GitHub username?"
       }
+
     ]).then(answers => {
       const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
       teamArray.push(engineer);
       createTeam();
     });
+
   }
 
   function addIntern() {
     inquirer.prompt([
-
+      
       {
         type: "input",
         name: "internName",
@@ -127,13 +149,19 @@ function addEngineer() {
 
   // Would you like to add a team member?
   // Yes || No
-   // If Yes --> Then select an employee role for your new team member: Manager, Engineer, Intern
+  // If Yes --> Then select an employee role for your new team member: Manager, Engineer, Intern
   // If No --> Create Team
 
 
-
-function createTeam () {
+function htmlBuilder () {
     console.log("Team created!")
-}
+
+    fs.writeFileSync(outputPath, generateTeam(teamArray), "UTF-8")
 
 }
+
+createTeam();
+
+}
+
+runApp();
